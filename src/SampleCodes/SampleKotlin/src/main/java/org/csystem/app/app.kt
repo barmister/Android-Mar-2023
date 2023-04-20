a6408ef11e04c9a0b60441884511ab26d545eaa5
barishalimergun <121036466+barishalimergun@users.noreply.github.com>
2023-04-20 23:23:46 +0300

    notlar.txt eklendi.

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,16 +1,6 @@
-/*----------------------------------------------------------------------------------------------------------------------
-    String sınıfının tüm karakterleri for döngüsü ile elde edilebilir. Yani String sınıfı "iterable"'dır. Iterable kavramı
-    ileride ele alınacaktır
-----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app
 
 fun main()
 {
-    print("Bir yazı giriniz:")
-    val s = readln()
-
-    for (ch in s)
-        print("$ch ")
-
-    println()
+    println("Hello World!...")
 }
